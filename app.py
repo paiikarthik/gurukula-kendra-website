2b3365521c4005eb1dea19146bbd5431b319b408
karthik <karthikpai282@gmail.com>
2026-09-07 22:31:56 +0530

    adding comment fr better understandabili

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, request,jsonify,render_template
+from flask import Flask, request,jsonify,render_template #Flask  creates our web application
+# request → receives data from JavaScript
+# jsonify → sends a JSON response back
+# render_template → displays your HTML
 
 
-app= Flask(__name__)
+app= Flask(__name__)  #Creates the Flask application.
 
 
 @app.route("/")
 
 def home():
-    return render_template("index.html")
+    return render_template("index.html")  #first page index page
 
 
-@app.route("/regiter",methods=["POST"])
+@app.route("/register",methods=["POST"])  #r JavaScript will send the form data here.
 
 def register():
-    data-request.get_json()
+    data-request.get_json() #receives the JavaScript object.
 
     print("Received data:")
 
