6b0fce057215a01f75a89f173db13b3d89e219ea
karthik <karthikpai282@gmail.com>
2026-09-07 22:28:45 +0530

    Connecting to FLask Backend

--- app.py
+++ app.py
@@ -10,3 +10,20 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/regiter",methods=["POST"])
+
+def register():
+    data-request.get_json()
+
+    print("Received data:")
+
+
+    return jsonify(
+        {
+            "success": True,
+            "message": "Registration Sucessfully"
+        }
+    )
+
+if __name__=="__main__":
+    app.run(debug=True)
